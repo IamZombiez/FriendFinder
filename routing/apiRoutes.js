c09d4f0413be4a285304fd33fc78d2dd6c421fb5
IamZombiez <dereksiu216@gmail.com>
2017-04-26 17:11:37 -0700

    cleaned up previous commit

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,7 +1,4 @@
-app.get('/', function (req, res) {
-   res.sendFile(path.join(__dirname, "/public/home.html"));
+app.get('api/friends', function(req, res) {
+  res.sendFile(path.join(__dirname, "/app/data/friends.js"))
 });
- 
- app.get('/survey', function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/survey.html"));
-});
+
